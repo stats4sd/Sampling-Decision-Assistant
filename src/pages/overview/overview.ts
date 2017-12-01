e6071fa30785c09ebdfdc14ff2e6bc8dbc01f861
Dave Mills <d.e.mills@stats4sd.org>
2017-12-01 13:33:23 +0000

    Moving overview css into main app for reuse in other pages

--- src/pages/overview/overview.ts
+++ src/pages/overview/overview.ts
@@ -14,10 +14,10 @@ export class OverviewPage {
     
     this.sections=[
       {name:"General objectives",icon:"assets/img/icons/objectives.svg", page:"ObjectivesPage",number:1},
-      {name:"Indicators",icon:"assets/img/icons/indicators.svg", page:"IndicatorsPage",number:1},
-      {name:"Definition of the target population and units of study",icon:"assets/img/icons/population.svg", page:"TargetPopulationPage",number:1},
-      {name:"At what level do you need to report these results",icon:"assets/img/icons/reporting.svg", page:"ReportingPage",number:1},
-      {name:"Reaching the sampling units",icon:"assets/img/icons/outreach.svg", page:"OutreachPage",number:1, class:'disabled'},
+      {name:"Indicators",icon:"assets/img/icons/indicators.svg", page:"IndicatorsPage",number:2},
+      {name:"Definition of the target population and units of study",icon:"assets/img/icons/population.svg", page:"TargetPopulationPage",number:3},
+      {name:"At what level do you need to report these results",icon:"assets/img/icons/reporting.svg", page:"ReportingPage",number:4},
+      {name:"Reaching the sampling units",icon:"assets/img/icons/outreach.svg", page:"OutreachPage",number:5, class:'disabled'},
     ]
     console.log('sections', this.sections)
   }
