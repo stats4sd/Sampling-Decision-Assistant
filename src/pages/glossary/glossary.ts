a5ca75d407e6783952c5059b9f309dec57a0dcbc
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2017-12-07 21:13:34 +0000

    glossary internal link binding and updates

--- src/pages/glossary/glossary.ts
+++ src/pages/glossary/glossary.ts
@@ -8,41 +8,79 @@ import glossaryMaster from '../../models/glossaryTerms'
   templateUrl: 'glossary.html',
 })
 export class GlossaryPage {
-  glossaryTerms:any=glossaryMaster;
-  modalMode:boolean;
-  activeTerm:any;
+  glossaryTerms: any = glossaryMaster;
+  modalMode: boolean;
+  activeTerm: any={}
 
   constructor(public viewCtrl: ViewController, public navParams: NavParams) {
-    if(navParams.data.term){
-      this.modalMode=true
+    if (navParams.data.term) {
+      this.modalMode = true
       this._getActiveTerm(navParams.data.term)
     }
   }
 
-  dismiss(){
+  dismiss() {
     this.viewCtrl.dismiss()
   }
 
-  showFullGlossary(){
-    this.activeTerm=null
+  showFullGlossary() {
+    this.activeTerm = {}
   }
 
-  setActiveTerm(term){
-    console.log('setting active term',term)
-    if(!term.Definition){term.Definition="This is just a placeholder definition for "+term.Term+". More content will be added later"}
-    this.activeTerm=term
+  setActiveTerm(term) {
+    console.log('setting active term', term)
+    if (!term.Definition) { term.Definition = "This is just a placeholder definition for " + term.Term + ". More content will be added later" }
+    this.activeTerm = term
   }
-  
-  _getActiveTerm(term){
+
+  _getActiveTerm(term) {
     // get term from glossary and set as active
     this.glossaryTerms.forEach(el => {
-      if(el.Term==term){
-        this.activeTerm=el
-        return
+      if (el.Term == term) {
+        this.activeTerm = el
+        return "success"
       }
+      return "fail"
     });
   }
 
-  
+  _renderHtml(definition){
+    let content = document.getElementById('definition')
+    content.innerHTML=this.activeTerm.Definition
+    let links = Array.prototype.slice.call(content.querySelectorAll('a'));
+    for (let link of links) {
+      link.onclick = function (e) {
+        this._linkClick(link.href, link.text, e)
+      }.bind(this)
+    }
+  }
+
+  _setupClickHandlers() {
+    // handle link and image clicks to not follow href (opening external)
+    let content = document.getElementById('definition')
+    
+    
+    let images = Array.prototype.slice.call(content.querySelectorAll('img'));
+    for (let image of images) {
+      image.onclick = function (e) {
+        event.preventDefault()
+        this.imageClick(image.src, e.target)
+      }.bind(this)
+    }
+  }
+
+  _linkClick(href,text,e){
+    // not great way to test if local link or external
+    // better if just follow hyperlink and bind deeplinking structure to page
+    console.log('link',href)
+    console.log('location',window.location)
+    if(href.indexOf(window.location.origin)>-1){
+      event.preventDefault()
+      this._getActiveTerm(text)
+    }
+  }
+
+
+
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,7 +9,7 @@ import { DataProvider } from '../../providers/data/data'
 export class HomePage {
   sections: any = []
   altSections: any = [];
-  version:string="0.2.2c";
+  version:string="0.2.4";
 
   imageSrc="assets/img/feature-image-1.jpg"
   // no longer using slides
