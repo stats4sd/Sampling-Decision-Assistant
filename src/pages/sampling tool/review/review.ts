7c5cec0a3d07758d0a9380b86075f6af5b5ff9f0
laptop\chris <chris.m.clarke@live.co.uk>
2018-05-09 21:06:53 +0100

    tree diagram export

--- src/pages/sampling tool/review/review.ts
+++ src/pages/sampling tool/review/review.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChildren, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { IonicPage, NavController, NavParams, Slides, ModalController } from 'ionic-angular';
-import {DataProvider} from '../../../providers/data/data'
+import { DataProvider } from '../../../providers/data/data'
 import { NgRedux } from '@angular-redux/store';
 import { AppState } from '../../../models/models';
 
@@ -16,31 +16,33 @@ export class ReviewPage {
   @ViewChildren('survey') surveys;
   canvasImage: any;
   showIntro: boolean = true;
-  viewSection:number=0;
+  viewSection: number = 0;
 
 
-  constructor(private modalCtrl: ModalController, private dataPrvdr:DataProvider, private ngRedux:NgRedux<AppState>) {
+  constructor(private modalCtrl: ModalController, private dataPrvdr: DataProvider, private ngRedux: NgRedux<AppState>) {
     // load question meta from questionMeta.ts and seperate out into question groups for binding to survey question components
     console.log('generating question groups')
     this._generateQuestionGroups()
   }
-  
 
-  _generateQuestionGroups(){
+
+  _generateQuestionGroups() {
     let groups = {}
-    this.dataPrvdr.questionMeta.forEach(q=>{
-      if(!groups[q.section]){groups[q.section]={
-        section:q.section,
-        questions:[]
-      }}
+    this.dataPrvdr.questionMeta.forEach(q => {
+      if (!groups[q.section]) {
+        groups[q.section] = {
+          section: q.section,
+          questions: []
+        }
+      }
       groups[q.section].questions.push(q)
     })
-    console.log('groups',groups)
-    Object.keys(groups).forEach(k=>{
+    console.log('groups', groups)
+    Object.keys(groups).forEach(k => {
       const val = groups[k]
       this.questionGroups.push(val)
     })
-    console.log('question groups',this.questionGroups)
+    console.log('question groups', this.questionGroups)
   }
 
 
@@ -51,8 +53,8 @@ export class ReviewPage {
     })
     modal.present()
   }
- 
-  export(){
+
+  export() {
     this.dataPrvdr.export()
   }
 
@@ -60,15 +62,33 @@ export class ReviewPage {
     this.showIntro = false;
   }
 
-  exportJSON(){
+  exportTreeImage() {
     const project = this.ngRedux.getState().activeProject
-    const dataStr:string = JSON.stringify(project)
-    let dataUri = 'data:application/json;charset=utf-8,'+ encodeURIComponent(dataStr);
-    let exportFileDefaultName = project.title+'.json';
+    const canvas = document.querySelector('canvas');
+    console.log('canvas', canvas)
+    const dataURL = canvas.toDataURL('image/png');
+    let exportFileDefaultName = project.title + '.png';
+    let linkElement = document.createElement('a');
+    linkElement.setAttribute('href', dataURL);
+    linkElement.setAttribute('download', exportFileDefaultName);
+    linkElement.click();
+    console.log(dataURL);
+  }
+
+  exportJSON() {
+    const project = this.ngRedux.getState().activeProject
+    const dataStr: string = JSON.stringify(project)
+    let dataUri = 'data:application/json;charset=utf-8,' + encodeURIComponent(dataStr);
+    let exportFileDefaultName = project.title + '.json';
     let linkElement = document.createElement('a');
     linkElement.setAttribute('href', dataUri);
     linkElement.setAttribute('download', exportFileDefaultName);
     linkElement.click();
+
+  }
+
+  download(data,extension){
+
 
   }
 
