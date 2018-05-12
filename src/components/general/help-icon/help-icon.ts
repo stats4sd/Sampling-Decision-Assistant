409dd649f54680e2be44e08c9b508b8cffd9537d
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2018-05-12 12:18:39 +0100

    restyle help icon

--- src/components/general/help-icon/help-icon.ts
+++ src/components/general/help-icon/help-icon.ts
@@ -5,7 +5,14 @@ import { CustomRouterProvider } from '../../../providers/router/router';
 
 @Component({
   selector: 'help-icon',
-  templateUrl: 'help-icon.html'
+  template:
+  `
+  <div class="help-container" >
+    <button ion-button icon-only clear style="color:rgba(0,0,0,0.9); margin:0" (click)="helpClicked()">
+    <ion-icon style="padding:0 8px" ios="ios-help-circle-outline" md="ios-help-circle-outline"></ion-icon>
+    </button>
+  </div>
+`
 })
 export class HelpIconComponent {
 
