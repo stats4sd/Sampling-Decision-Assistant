0c6d867e0f5e6f13a3c9f9745530991083120092
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2018-10-01 10:29:43 +0100

    Fix sd errors, organise issues and waffle, test support for multiple offline tab sync

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,56 +1,60 @@
 // core
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { MyApp } from "./app.component";
 // animations
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AnimatorModule } from 'css-animator';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AnimatorModule } from "css-animator";
 // platform and providers
 // import { StatusBar } from '@ionic-native/status-bar';
 // import { SplashScreen } from '@ionic-native/splash-screen';
-import { IonicStorageModule } from '@ionic/storage';
-import { DataProvider } from '../providers/data/data';
-import { FormProvider } from '../providers/form/form';
+import { IonicStorageModule } from "@ionic/storage";
+import { DataProvider } from "../providers/data/data";
+import { FormProvider } from "../providers/form/form";
 // menu
-import { DecisionToolMenuComponent } from '../components/general/decision-tool-menu/decision-tool-menu'
+import { DecisionToolMenuComponent } from "../components/general/decision-tool-menu/decision-tool-menu";
 // redux
-import { NgRedux, DevToolsExtension, NgReduxModule } from '@angular-redux/store';
-import { AppState } from '../models/models';
-import { rootReducer, INITIAL_STATE } from '../reducers/reducers';
-import { ProjectActions, DevActions, ViewActions, TreeDiagramActions } from '../actions/actions';
+import {
+  NgRedux,
+  DevToolsExtension,
+  NgReduxModule
+} from "@angular-redux/store";
+import { AppState } from "../models/models";
+import { rootReducer, INITIAL_STATE } from "../reducers/reducers";
+import {
+  ProjectActions,
+  DevActions,
+  ViewActions,
+  TreeDiagramActions
+} from "../actions/actions";
 // dev db
-import { AngularFireModule } from 'angularfire2';
-import { firebaseConfig } from '../components/_dev/config';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { CustomRouterProvider } from '../providers/router/router';
-import { SentryErrorHandler } from '../providers/error-handler/error-handler';
-import { GlossaryProvider } from '../providers/glossary/glossary';
-import { ResourcesProvider } from '../providers/resources/resources';
-import { DataVisProvider } from '../providers/data-vis/data-vis';
+import firebase from "firebase";
+import { AngularFireModule } from "angularfire2";
+import { firebaseConfig } from "../components/_dev/config";
+import { AngularFirestoreModule } from "angularfire2/firestore";
+import { CustomRouterProvider } from "../providers/router/router";
+import { SentryErrorHandler } from "../providers/error-handler/error-handler";
+import { GlossaryProvider } from "../providers/glossary/glossary";
+import { ResourcesProvider } from "../providers/resources/resources";
+import { DataVisProvider } from "../providers/data-vis/data-vis";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    DecisionToolMenuComponent
-  ],
+  declarations: [MyApp, DecisionToolMenuComponent],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp, { preloadModules: true }),
     IonicStorageModule.forRoot({
-      name: '__sampling',
+      name: "__sampling"
     }),
     AnimatorModule,
     BrowserAnimationsModule,
     NgReduxModule,
-    AngularFireModule.initializeApp(firebaseConfig.firebase,'v1'),
-    AngularFirestoreModule.enablePersistence(),
+    AngularFireModule.initializeApp(firebaseConfig.firebase, "v1"),
+    AngularFirestoreModule.enablePersistence()
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    DecisionToolMenuComponent
-  ],
+  entryComponents: [MyApp, DecisionToolMenuComponent],
   providers: [
     // StatusBar,
     // SplashScreen,
@@ -66,7 +70,7 @@ import { DataVisProvider } from '../providers/data-vis/data-vis';
     CustomRouterProvider,
     GlossaryProvider,
     ResourcesProvider,
-    DataVisProvider,
+    DataVisProvider
   ]
 })
 export class AppModule {
@@ -77,6 +81,7 @@ export class AppModule {
       INITIAL_STATE,
       [],
       // [reduxLogger.createLogger()],
-      devTools.isEnabled() ? [devTools.enhancer()] : []);
+      devTools.isEnabled() ? [devTools.enhancer()] : []
+    );
   }
 }
--- src/components/stage-content/main-content/2/stage-2.ts
+++ src/components/stage-content/main-content/2/stage-2.ts
@@ -1,67 +1,76 @@
-import { Component } from '@angular/core';
-import { Events } from 'ionic-angular';
-import { StagePage } from '../../../../pages/sampling tool/stage/stage';
-import { DecimalPipe } from '@angular/common';
-import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { select } from '@angular-redux/store'
-
+import { Component } from "@angular/core";
+import { StagePage } from "../../../../pages/sampling tool/stage/stage";
+import { Subject, Subscription } from "rxjs";
 
 @Component({
-  selector: 'stage-2',
-  templateUrl: 'stage-2.html'
+  selector: "stage-2",
+  templateUrl: "stage-2.html"
 })
 export class Stage2Component extends StagePage {
-  @select(['activeProject', 'values', 'q2.2.3']) readonly sdLower$: Observable<number>;
-  @select(['activeProject', 'values', 'q2.2.4']) readonly sdUpper$: Observable<number>;
-
+  private componentDestroyed: Subject<any> = new Subject();
+  // could use select, but as data not streamed into template becomes messier for unsubscribe
+  sdLower$: Subscription;
+  sdUpper$: Subscription;
   sd: number;
 
   ngOnInit() {
-    this.sdLower$.subscribe(v => { this._calculateSD() })
-    this.sdUpper$.subscribe(v => { this._calculateSD() })
-    // listen for updates on min/max values to automatically calculate s.d
-    // note subscribing on redux as overrids over subscribers
-    // this.form.valueChanges.subscribe(v => {
-    //   if (v && v['q2.2.3'] && v['q2.2.4']) { this._calculateSD(v['q2.2.3'], v['q2.2.4'], this.sd) }
-    // })
-    // calculate now also
-    try {
-      let v = this.ngRedux.getState().activeProject.values
-      if (v['q2.2.3'] && v['q2.2.4']) { this._calculateSD(v['q2.2.3'], v['q2.2.4']) }
-    } catch (error) {
-    }
+    this._addSubsriptions();
+    this._calculateSD();
+  }
 
+  ngOnDestory() {
+    this.componentDestroyed.next();
+    this.componentDestroyed.unsubscribe();
+    console.log("subscription removed?", this.sdLower$);
+  }
+
+  // manage subscribe and unsubscribe in declarative way
+  // see: https://netbasal.com/when-to-unsubscribe-in-angular-d61c6b21bad3
+  _addSubsriptions() {
+    // listen for updates on min/max values to automatically calculate s.d
+    this.sdLower$ = this.ngRedux
+      .select(["activeProject", "values", "q2.2.3"])
+      .takeUntil(this.componentDestroyed)
+      .subscribe(v => {
+        this._calculateSD();
+      });
+    this.sdUpper$ = this.ngRedux
+      .select(["activeProject", "values", "q2.2.4"])
+      .takeUntil(this.componentDestroyed)
+      .subscribe(v => {
+        this._calculateSD();
+      });
   }
   _calculateSD(lower?, upper?) {
     try {
-      const values = this.ngRedux.getState().activeProject.values
-      if (!lower) { lower = parseFloat(values['q2.2.3']) }
-      if (!upper) { upper = parseFloat(values['q2.2.4']) }
-      if (lower && upper) {
-        let sd = (upper - lower) / 6
-        // round to 2dp with scaling support (avoid long float numbers)
-        sd = Math.round((sd + 0.00001) * 100) / 100
-        this.sd = sd
-        if (sd != parseFloat(values['q2.2.2'])) {
-          let patch = {}
-          // save as string to keep consistent with inputs (even though later converted back to number)
-          patch['q2.2.2'] = sd.toString()
-          // patch only works if exists so also provide option to add control
-          if (!values['q2.2.2']) {
-            this.formPrvdr.formGroup.addControl('q2.2.2', this.formPrvdr.fb.control(''))
-          }
-          this.formPrvdr.formGroup.patchValue(patch)
-          this.dataPrvdr.backgroundSave()
-        }
-
+      const values = this.ngRedux.getState().activeProject.values;
+      if (!lower) {
+        lower = Number(values["q2.2.3"]);
       }
-
-    } catch (error) {
-      
-    }
+      if (!upper) {
+        upper = Number(values["q2.2.4"]);
+      }
+      console.log("calculating sd", lower, upper);
+      let sd = (upper - lower) / 6;
+      // round to 2dp with scaling support (avoid long float numbers)
+      sd = Math.round((sd + 0.00001) * 100) / 100;
+      this.sd = sd;
+      if (sd != parseFloat(values["q2.2.2"])) {
+        let patch = {};
+        // save as string to keep consistent with inputs (even though later converted back to number)
+        patch["q2.2.2"] = sd.toString();
+        // patch only works if exists so also provide option to add control
+        if (!values["q2.2.2"]) {
+          this.formPrvdr.formGroup.addControl(
+            "q2.2.2",
+            this.formPrvdr.fb.control("")
+          );
+        }
+        this.formPrvdr.formGroup.patchValue(patch);
+        this.dataPrvdr.backgroundSave();
+      }
+    } catch (error) {}
 
     // calculate sd, only update form value if different to previous
   }
-
 }
