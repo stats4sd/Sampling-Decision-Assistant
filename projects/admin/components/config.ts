1b2b57701e76e459d658f84a9f656e91dec1902e
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2019-12-19 13:14:36 +0000

    add placeholder config

--- projects/admin/components/config.ts
+++ projects/admin/components/config.ts
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
-    production: false,
-    firebase: {
-        apiKey: "AIzaSyCtv1otoXgRf0eURZbFCzdJbA_z7MVJ81Y",
-        authDomain: "unhcr-sampling-assistant.firebaseapp.com",
-        databaseURL: "https://unhcr-sampling-assistant.firebaseio.com",
-        projectId: "unhcr-sampling-assistant",
-        storageBucket: "unhcr-sampling-assistant.appspot.com",
-        messagingSenderId: "701074462069"
-      }
-}
+  production: false,
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+  }
+};
